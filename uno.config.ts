7a373ec6374bd75d7f74206b4303405903c362a6
Franciszek Czajka <franekczajkaa@gmail.com>
2024-02-16 19:56:48 +0100

    Add and set up UnoCSS

---
+++ uno.config.ts
@@ -0,0 +1,41 @@
+// uno.config.ts
+import {
+    defineConfig,
+    presetUno,
+    presetAttributify,
+    presetWebFonts,
+    presetIcons,
+  } from "unocss";
+
+export default defineConfig({presets: [
+    presetUno(),
+    presetAttributify({
+      prefix: "un-",
+      strict: true,
+      prefixedOnly: true,
+    }),
+    presetIcons({
+      collections: {
+        mdi: () =>
+          import("@iconify-json/mdi/icons.json").then((i) => i.default),
+      },
+      extraProperties: {
+        display: "inline-block",
+        "vertical-align": "middle",
+      },
+    }),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        default: [
+          { name: "Fredoka", weights: ["400"] },
+          { name: "sans-serif", provider: "none" },
+        ],
+        headline: [
+          { name: "Protest Strike", weights: ["400"] },
+          { name: "display", provider: "none" },
+        ],
+      },
+    }),
+  ],
+})
