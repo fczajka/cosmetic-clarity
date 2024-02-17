e11607c589eb1bce5b506a16adaf7ad67da517d4
Franciszek Czajka <franekczajkaa@gmail.com>
2024-02-17 18:09:52 +0100

    Add simple search

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { type FormEvent, useState } from "react";
 import { Layout } from "./components";
+import Data from "./public/data.json";
 
 export default function App() {
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const [ingridients, setIngridients] = useState<string>("");
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    const ingridientsArr = ingridients.split(",");
+    const trimmedIngridients = ingridientsArr.map((ingridient) =>
+      ingridient.trim().toLowerCase(),
+    );
+
+    const result = Data.filter((item) => {
+      return trimmedIngridients.includes(item.title.toLowerCase());
+    });
+
+    // CODE FOR NOT USING EXACT MATCH
+    // const result = trimmedIngridients.map((ingridient) => {
+    //   const matchingIngridients = Data.filter((item) => {
+    //     return item.title.toLowerCase().includes(ingridient);
+    //   });
+    //   return matchingIngridients;
+    // });
   }
   return (
     <Layout>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="" id="" />
+        <textarea
+          onChange={(e) => setIngridients(e.target.value)}
+          value={ingridients}
+        />
         <button type="submit">Check</button>
       </form>
     </Layout>
